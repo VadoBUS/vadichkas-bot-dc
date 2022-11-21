4e6663cb35a83c86cc1134fd7785e171358e2090
VadoBUS <116962736+VadoBUS@users.noreply.github.com>
2022-11-21 19:04:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -72,3 +72,56 @@ channel: "1044276276216725544",
 executeOnStartup: true,
 every: 500000
 })
+
+bot.interactionCommand({
+name: "play",
+  code: `
+  $if[$channelID==1044232414110683156]
+  $title[Найдено:]
+  $description[$playSong[$message]]
+  $color[0000ff]
+  $footer[$username;$authorAvatar]
+  $else
+  $title[<a:no:840961533848846356> Ошибка!]
+	$description[-В данном канале нельзя выполнить эту команду]
+	$color[ff0000]
+	$footer[$username;$authorAvatar]
+  $endif`
+
+})
+bot.interactionCommand({
+name: "eval",
+  code: `
+  $if[$authorID==852906146385231872]
+	$eval[$message]
+	$else
+	$title[<a:no:840961533848846356> Ошибка!]
+	$description[-Вы не являетесь создателем данного бота!]
+	$color[ff0000]
+		$endif`
+
+})
+bot.interactionCommand({
+name: "clear",
+  code: `
+  $if[$message!=]
+  $clear[$message]
+  $title[Очистка чата]
+  $description[Участник \`$username\` очистил чат на \`$message\` сообщений]
+  $color[0000аа]
+  $else
+$title[<a:no:840961533848846356> Ошибка!]
+	$description[-Вы забыли написать кол-во удаляемыъ сообщений]
+	$color[ff0000]
+	$footer[$username;$authorAvatar]
+  $endif`
+
+})
+bot.interactionCommand({
+name: "ping",
+  code: `$title[Pong!]
+	$description[$ping]
+	$color[0000FF]
+	$footer[$username;$authorAvatar]`
+
+})
