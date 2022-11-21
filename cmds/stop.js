9d5c355c5b9cc413ad2dc130dbe40eeb5dc3b33b
VadoBUS <116962736+VadoBUS@users.noreply.github.com>
2022-11-21 18:55:27 +0300

    Update stop.js

--- cmds/stop.js
+++ cmds/stop.js
@@ -1,17 +1,5 @@
 module.exports = {
 	name: "stop",
 	aliases: ['s'],
-  code: `
-$if[$channelID==1044232414110683156]
-  $title[Аривидерчи]
-  $description[Пока мой друг]
-  $color[0000ff]
-  $footer[$username;$authorAvatar]
-  $stopSong
-  $else
-  $title[<a:no:840961533848846356> Ошибка!]
-	$description[-В данном канале нельзя выполнить эту команду]
-	$color[ff0000]
-	$footer[$username;$authorAvatar]
-  $endif`
+  code: `$createSlashCommand[$guildID;stop;Stop music]`
 }
